6aea8fe01f7f8c1f15f73cd310f77ebc7d4272b6
Mandar Ambulkar <68551215+Mandar77@users.noreply.github.com>
2025-02-13 19:24:20 -0800

    Time: 0 ms (100%), Space: 7.5 MB (85.19%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -0,0 +1,10 @@
+uint32_t reverseBits(uint32_t n) {
+    uint32_t result = 0;
+    
+    for (int i = 0; i < 32; i++) {
+        result = (result << 1) | (n & 1);  // Shift result left and add LSB of n
+        n >>= 1;  // Shift n right to process the next bit
+    }
+    
+    return result;
+}
