4dedcc655f2f1b8ec39e1ca5b9e8722e4ce223fe
Mandar Ambulkar <68551215+Mandar77@users.noreply.github.com>
2025-02-13 19:23:12 -0800

    Time: 18 ms (5.61%), Space: 11.6 MB (100%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def numDecodings(self, s):
+        if not s or s[0] == '0':
+            return 0
+        
+        n = len(s)
+        
+        # Initialize an array to store the number of ways to decode up to the ith character
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1 if s[0] != '0' else 0
+        
+        # Fill the array using the recurrence relation
+        for i in range(2, n + 1):
+            one_digit = int(s[i - 1:i])
+            two_digits = int(s[i - 2:i])
+            
+            if 1 <= one_digit <= 9:
+                dp[i] += dp[i - 1]
+            
+            if 10 <= two_digits <= 26:
+                dp[i] += dp[i - 2]
+        
+        return dp[n]
+            
